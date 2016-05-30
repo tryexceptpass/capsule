cb8b20d908b8f4c12882b9b9875ac09176b333d4
tryexceptpass <@tryexceptpass>
2016-05-29 17:35:27 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     author_email = "tryexceptpass@users.noreply.github.com",
 
     name = "capsule",
-    version = "0.2.0",
+    version = "0.2.1",
     description = "Manage development environments like virtualenv but with docker containers and export your python history to iPythonNotebook",
-    long_description="This package is meant to help manage development environments using docker, while providing an interface similar to that of python's virtualenvwrapper module.\n\nFeatures:\n\n* Simple cli to create, switch to and delete environments that allow you to fiddle to your heart's content without breaking anything else.\n\n* Currently this is based on a clean Ubuntu install (ubuntu:latest docker image) with basic python 2.7 configured.\n\n* Save your environment to a tarfile in case you want to take it with you anywhere.\n\n* Export the python history of anything you tried while using the python interpreter within your environment into an iPythonNotebook file that can be used with any Jupyter server (like the jupyter/notebook docker image).",
+    long_description="This package is meant to help manage development environments using docker, while providing an interface similar to that of python's virtualenvwrapper module.\n\nFeatures:\n\n* Simple cli to create, switch to and delete environments that allow you to fiddle to your heart's content without breaking anything else.\n\n* Currently this is based on a clean Ubuntu install (ubuntu:latest docker image) with basic python 2.7 configured.\n\n* Save your environment to a tarfile in case you want to take it with you anywhere.\n\n* Export the python history of anything you tried while using the python interpreter within your environment into a JupyterNotebook file that can be used with any Jupyter server (like the jupyter/notebook docker image).",
 
     url = "https://github.com/tryexceptpass/capsule",
 
